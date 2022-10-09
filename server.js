066c8f7e9c6973fbbe092ea824f1cc57419f1bb5
briskrem <crissjohndoe@gmail.com>
2022-10-09 18:03:42 -0400

    p3

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const app = require('./index')
+const PORT = process.env.PORT || 3003
 
-const server = app.listen(3002, ()=>{
-    console.log('connected on port 3002')
+const server = app.listen(PORT, ()=>{
+    console.log(`connected on port ${PORT}`)
 })
 
 module.exports = server
